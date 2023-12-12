33fa66ed1ab6ea8fed944a39a7f418a84fad89e6
Petro <xavrais312@gmail.com>
2023-12-12 14:37:20 +0200

    expanses list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expenses_app/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expenses_app/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,8 +13,15 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Test1', amount: 11.1, date: DateTime.now(), category: Category.market),
+        Expense(title: 'Test2', amount: 21.1, date: DateTime.now(), category: Category.caffe)
+
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Column(children: [Text('chart'), Text('body')]),);
+    return Scaffold(body: Column(children: [Text('chart'), Expanded(child: ExpensesList(expenses: _registeredExpenses))]),);
   }
 }
