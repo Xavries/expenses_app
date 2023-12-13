8e731260a6bca082171953606a757c6e8ecfe21e
Petro <xavrais312@gmail.com>
2023-12-13 16:43:31 +0200

    show datepicker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,14 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
+  void _openDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 5, now.month, now.day);
+    final lastDate = DateTime(now.year + 1, now.month, now.day);
+
+    showDatePicker(context: context, firstDate: firstDate, lastDate: lastDate);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -33,13 +41,30 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Назва витрат')
             ),
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: _amountController,
-            maxLength: 20,
-            decoration: const InputDecoration(
-              label: Text('Сума')
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  maxLength: 20,
+                  decoration: const InputDecoration(
+                    label: Text('Сума')
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16,),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('selected date'),
+                    IconButton(onPressed: _openDatePicker, icon: Icon(Icons.calendar_month))
+                  ],
+                )
+                )
+            ],
           ),
           Row(
             children: [
