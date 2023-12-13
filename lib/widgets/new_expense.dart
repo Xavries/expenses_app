6cab476720f8ac998021a5c38561a599e6a1813c
Petro <xavrais312@gmail.com>
2023-12-13 16:28:37 +0200

    more fields and buttons to new expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,18 +10,48 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
-              label: Text('Title')
+            decoration: const InputDecoration(
+              label: Text('Назва витрат')
             ),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: _amountController,
+            maxLength: 20,
+            decoration: const InputDecoration(
+              label: Text('Сума')
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {print('fff');},
+                child: Text('Закрити')
+              ),
+              ElevatedButton(
+                onPressed: () {print('fff');},
+                child: Text('Зберегти')
+              ),
+            ],
           )
         ],
       ),
