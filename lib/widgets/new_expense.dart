9beb046ab4f6fa48747a6a80b575ef63d56bcc29
Petro <xavrais312@gmail.com>
2023-12-13 17:10:48 +0200

    dropdawn button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.caffe; 
 
   void _openDatePicker() async {
     final now = DateTime.now();
@@ -72,9 +73,25 @@ class _NewExpenseState extends State<NewExpense> {
                 )
             ],
           ),
+          const SizedBox(height: 16,),
           Row(
             children: [
-              ElevatedButton(
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values.map(
+                  (category) => DropdownMenuItem(value: category, child: Text(category.name.toUpperCase()))
+                  ).toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                }
+                ),
+              const Spacer(),
+              TextButton(
                 onPressed: () {Navigator.pop(context);},
                 child: Text('Закрити')
               ),
